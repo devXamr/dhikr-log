d3b88f8c827af2ae012bfb04d06c02318c645112
devXamr <devxamr@gmail.com>
2025-03-02 12:15:12 +0530

    fixed button accessibility issue by changing from div to button elements across the site

--- src/DarkModeToggleButton.jsx
+++ src/DarkModeToggleButton.jsx
@@ -3,9 +3,9 @@ import { MdOutlineLightMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 
 export default function DarkModeToggleButton({boolVal, setterFunc}){
-    return <div className={`${boolVal && 'dark'} absolute right-4 top-3 border px-4 py-1.5 rounded-lg dark:text-white`} onClick={setterFunc} >
+    return <button       className={`${boolVal && 'dark'} absolute right-4 top-3 border px-4 py-1.5 rounded-lg dark:text-white`} onClick={setterFunc} >
        <MdDarkMode/>
 
-    </div>
+    </button>
 
 }
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -20,8 +20,8 @@ export default function Landing({isDarkOn, setDark}){
 
 
         <div className='text-center pt-72 font-semibold text-green-700 text-xl dark:text-green-400'>Choose Your Option</div>
-        <div onClick={simpleDirect} className='pt-4 pr-6 pl-6 pb-4 hover:w-52 border-white border-4 hover:shadow-md hover:border-green-800 text-center mt-7 w-48 mr-auto ml-auto rounded-xl bg-gray-200 hover:cursor-pointer duration-200 dark:bg-gray-600 dark:text-white dark:font-bold dark:border-gray-700'>Simple Counter</div>
-        <div onClick={tasbihDirect} className='pt-4 pr-8 pl-8 pb-4 hover:shadow-md text-center mt-7 w-48 mr-auto ml-auto rounded-xl bg-gray-200 hover:w-52 border-4 border-white hover:border-green-800 hover:cursor-pointer duration-200 dark:bg-gray-600 dark:text-white dark:font-bold dark:border-gray-700'>Tasbih</div>
+        <button onClick={simpleDirect} className='block pt-4 pr-6 pl-6 pb-4 hover:w-52 border-white border-4 hover:shadow-md hover:border-green-800 text-center mt-7 w-48 mr-auto ml-auto rounded-xl bg-gray-200 hover:cursor-pointer duration-200 dark:bg-gray-600 dark:text-white dark:font-bold dark:border-gray-700'>Simple Counter</button>
+        <button onClick={tasbihDirect} className='block pt-4 pr-8 pl-8 pb-4 hover:shadow-md text-center mt-7 w-48 mr-auto ml-auto rounded-xl bg-gray-200 hover:w-52 border-4 border-white hover:border-green-800 hover:cursor-pointer duration-200 dark:bg-gray-600 dark:text-white dark:font-bold dark:border-gray-700'>Tasbih</button>
     </div>
     </div>
 }
