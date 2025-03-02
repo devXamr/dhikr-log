391a84a67316410f0beda7e5d74c4688c3d6ad3a
devXamr <devxamr@gmail.com>
2025-03-02 21:40:01 +0530

    fixed branch issues

--- src/CountingTasbih.jsx
+++ src/CountingTasbih.jsx
@@ -101,69 +101,113 @@ export default function CountingTasbih({isDarkOn, setDark}){
     }
 
 
-    return (
-        <div className={`${isDarkOn && 'dark'} dark:bg-black font-cabinet overflow-hidden`}>
-            {/*<DarkModeToggleButton boolVal={isDarkOn} setterFunc={setDark}/>*/}
-            <button onClick={handleBackClick} className='text-5xl fixed top-3 left-3 text-[#9F8170]'><IoMdArrowRoundBack/></button>
-            <div className='w-screen h-screen z-10 dark:bg-black pt-48' onClick={updateTasbih}>
+  return (
+    <div
+      className={`${isDarkOn && "dark"} dark:bg-black font-cabinet overflow-hidden`}
+    >
+      {/*<DarkModeToggleButton boolVal={isDarkOn} setterFunc={setDark}/>*/}
+      <button
+        onClick={handleBackClick}
+        className="text-5xl fixed top-3 left-3 text-[#9F8170]"
+      >
+        <IoMdArrowRoundBack />
+      </button>
+      <div
+        className="w-screen h-screen z-10 dark:bg-black pt-48"
+        onClick={updateTasbih}
+      >
+        <button className="mx-auto w-fit block        ">
+          <GrPowerReset
+            onClick={resetTasbih}
+            className="ml-auto mr-auto text-4xl text-[#9F8170] dark:text-gray-500"
+          />
+        </button>
 
+        <div className="font-orbitron text-center text-7xl text-[#9F8170] mt-14 dark:text-gray-400">
+          {tasbih}
+        </div>
 
+        {tasbih === 0 && (
+          <div className="font-sans font-light text-lg text-center pt-12 dark:text-white">
+            Tap To Start
+          </div>
+        )}
 
-                <button className='mx-auto w-fit block        '>
-                    <GrPowerReset onClick={resetTasbih} className='ml-auto mr-auto text-4xl text-[#9F8170] dark:text-gray-500'/>
-                </button>
+        {tasbih > 0 && (
+          <button
+            className="block font-bold text-center  text-[#9F8170] bg-[#efdecd] w-max mt-11 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl border-4 border-white shadow-sm hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700"
+            onClick={changeSave}
+          >
+            Save
+          </button>
+        )}
 
-
-                <div className='font-orbitron text-center text-7xl text-[#9F8170] mt-14 dark:text-gray-400'>{tasbih}</div>
-
-                {tasbih === 0 && <div className='font-sans font-light text-lg text-center pt-12 dark:text-white'>Tap To Start</div>}
-
-                {tasbih > 0 && <button
-                    className='block font-bold text-center  text-[#9F8170] bg-[#efdecd] w-max mt-11 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl border-4 border-white shadow-sm hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700' onClick={changeSave}>Save</button>}
-
-
-                <button onClick={changeLog} className='block font-bold text-center shadow-sm w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl mb-8 border-4 border-white hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700 text-[#9F8170] bg-[#efdecd]'>Show Log</button>
-                { isLogClicked &&
-                    <motion.div
-                        initial={{opacity: 0, y: 1000}}
-                        animate={{opacity: 100, y: 0}}
-                        transition={{ease: "easeInOut"}}
-                        className='mr-1 font-cabinet ml-1 relative bg-white h-screen bottom-96 pt-7 rounded-t-2xl dark:bg-black'>
-                        <div className='flex justify-between font-bold'>
-                            <RowComp text='Date'/>
-                            <RowComp text='Label'/>
-                            <RowComp text='Count'/>
-                        </div>
-
-                        {allLogs.map(log => <OneFullRow date={log.date} lab={log.label} count={log.count}/>)}
-                        <button onClick={changeLog}
-                                className='block font-bold text-center shadow-sm w-max mt-8 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl mb-3 border-4 border-white hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700 text-[#9F8170] bg-[#efdecd]'>Close Logs
-                        </button>
-                        {allLogs && <div
-                            className='font-bold text-center bg-white border-4 border-red-600 text-red-600 hover:text-white duration-200 hover:cursor-pointer hover:bg-red-600 w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg'
-                            onClick={deleteLogs}>Delete All</div>}
-
-                    </motion.div>
-
-                }
+        <button
+          onClick={changeLog}
+          className="block font-bold text-center shadow-sm w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl mb-8 border-4 border-white hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700 text-[#9F8170] bg-[#efdecd]"
+        >
+          Show Log
+        </button>
+        {isLogClicked && (
+          <motion.div
+            initial={{ opacity: 0, y: 1000 }}
+            animate={{ opacity: 100, y: 0 }}
+            transition={{ ease: "easeInOut" }}
+            className="mr-1 font-cabinet ml-1 relative bg-white h-screen bottom-96 pt-7 rounded-t-2xl dark:bg-black"
+          >
+            <div className="flex justify-between font-bold">
+              <RowComp text="Date" />
+              <RowComp text="Label" />
+              <RowComp text="Count" />
             </div>
 
-            {isSaveClicked && <div
-                className='dark:text-white dark:bg-gray-600 z-50 bg-gray-200 absolute top-80 left-16 text-center pr-7 pl-7 pb-16 pt-16 rounded-xl mr-auto ml-auto'>
-                <div className='text-xl font-bold mb-3'>Save As:</div>
-                <input type='text' value={name} onChange={updateName} placeholder='Enter name' className='py-2 rounded-xl px-3 dark:bg-gray-400 dark:text-white dark:font-semibold' />
-                <div onClick={handleLogSubmit}
-                    className='font-bold text-center bg-white text-black w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl border-4 border-white hover:border-green-800 hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-black dark:border-gray-800'>Done
-                </div>
-                <div onClick={changeSave} className='font-bold text-center w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl hover:px-5 bg-white border-4 border-red-600 text-red-600 hover:text-white duration-200 hover:cursor-pointer hover:bg-red-600'>Cancel</div>
+            {allLogs.map((log) => (
+              <OneFullRow date={log.date} lab={log.label} count={log.count} />
+            ))}
+            <button
+              onClick={changeLog}
+              className="block font-bold text-center shadow-sm w-max mt-8 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl mb-3 border-4 border-white hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700 text-[#9F8170] bg-[#efdecd]"
+            >
+              Close Logs
+            </button>
+            {allLogs && (
+              <div
+                className="font-bold text-center bg-white border-4 border-red-600 text-red-600 hover:text-white duration-200 hover:cursor-pointer hover:bg-red-600 w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg"
+                onClick={deleteLogs}
+              >
+                Delete All
+              </div>
+            )}
+          </motion.div>
+        )}
+      </div>
 
-
-
-
-            </div>}
-
-
-        </div>
-    )
-
+      {isSaveClicked && (
+          <div
+              className="dark:text-white bg-[#efdecd] dark:bg-gray-600 z-50 fixed inset-9 top-48 h-max text-center px-9 py-16 rounded-xl mx-auto text-[#9F8170]">
+            <div className="text-xl font-bold mb-3">Save As:</div>
+            <input
+                type="text"
+                value={name}
+                onChange={updateName}
+                autoFocus={true}
+                placeholder="Eg. Astaghfirullah - Maghrib"
+                className="py-2 rounded-xl px-5 dark:bg-gray-400 dark:text-white dark:font-semibold w-full"
+            />
+            <button
+                className="block font-bold text-center  text-[#9F8170] bg-[#efdecd] w-max mt-11 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl border-4 border-white shadow-sm hover:cursor-pointer duration-200 hover:px-5 dark:text-white dark:bg-gray-600 dark:border-gray-700"
+                onClick={handleLogSubmit}
+            >
+              Done
+            </button>
+            <div
+                onClick={changeSave}
+                className="font-bold text-center w-max mt-4 mr-auto ml-auto pr-3 pl-3 pt-1 pb-1 rounded-lg text-xl hover:px-5 bg-white border-4 border-red-600 text-red-600 hover:text-white duration-200 hover:cursor-pointer hover:bg-red-600"
+            >
+              Cancel
+            </div>
+          </div>
+      )}
+    </div>
+  );
 }
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,71 +1,97 @@
-import {useNavigate} from "react-router-dom";
-import DarkModeToggleButton from "./DarkModeToggleButton.jsx";
-import waves from './assets/layered-waves-haikei (1).svg'
-import wavesmobile from './assets/layered-waves-haikei-mobile.svg'
-import lantern from './assets/islamic-lantern-svgrepo-com.svg'
-import sparkles from './assets/sparkles.svg'
-import {motion} from  'motion/react'
-import {MoveRight} from "lucide";
+import { useNavigate } from "react-router-dom";
+import wavesmobile from "./assets/layered-waves-haikei-mobile.svg";
+import lantern from "./assets/islamic-lantern-svgrepo-com.svg";
+import sparkles from "./assets/sparkles.svg";
+import { motion } from "motion/react";
 import { BsArrowRight } from "react-icons/bs";
-import { GiTronArrow } from "react-icons/gi"
-import { PiArrowBendRightUp } from "react-icons/pi"
 
+export default function Landing({ isDarkOn, setDark }) {
+  const navigate = useNavigate();
+  function simpleDirect() {
+    navigate("/simple");
+  }
 
-
-
-export default function Landing({isDarkOn, setDark}){
-
-    const navigate = useNavigate()
-    function simpleDirect(){
-        navigate('/simple')
-    }
-
-    // temporarily disabled.
-    function tasbihDirect(){
-        navigate('/tasbih')
-    }
-    return <div className={`${isDarkOn && 'dark'} font-cabinet`}>
-
-
-        {/*<DarkModeToggleButton boolVal={isDarkOn} setterFunc={setDark}/> */}
-        <div className='h-screen w-screen dark:bg-black relative '>
-
-
-            <div className='text-center pt-72 font-semibold text-[#9F8170] text-6xl dark:text-[#9F8170]'>Ramadan Kareem
-            </div>
-            <button onClick={simpleDirect}
-                    className='flex pt-4 pr-6 pl-6 pb-4  border-white border-4 hover:shadow-md shadow-sm text-center mt-7 w-fit mr-auto ml-auto rounded-xl bg-[#efdecd] hover:cursor-pointer duration-200 dark:bg-gray-600 dark:text-white dark:font-bold dark:border-gray-700'>
-
-                <div className='mr-4 text-lg text-[#9F8170] font-bold'>Open Counter</div>
-                <BsArrowRight className='mt-0.5 font-black text-2xl text-[#9F8170]'/>
-
-            </button>
-
-            <div className='text-center text-lg mt-10'>إِنَّ ٱللَّهَ يُحِبُّ ٱلتَّوَّٲبِينَ وَيُحِبُّ ٱلۡمُتَطَهِّرِينَ </div>
-            <div className='text-center text-lg max-w-56 mx-auto'>"Verily, Allah loves those who repent and those who purify themselves." [Al-Qur'an 2:222]</div>
-
-
-
-
+  // temporarily disabled.
+  function tasbihDirect() {
+    navigate("/tasbih");
+  }
+  return (
+    <div className={`${isDarkOn && "dark"} font-cabinet`}>
+      {/*<DarkModeToggleButton boolVal={isDarkOn} setterFunc={setDark}/> */}
+      <div className="h-screen w-screen dark:bg-black relative ">
+        <div className="md:text-[#efdecd] text-center pt-72 font-semibold text-[#9F8170] text-6xl dark:text-[#9F8170]">
+          Ramadan Kareem
         </div>
+        <button
+          onClick={simpleDirect}
+          className="flex pt-4 pr-6 pl-6 pb-4  border-white border-4 hover:shadow-md shadow-sm text-center mt-7 w-fit mr-auto ml-auto rounded-xl bg-[#efdecd] hover:cursor-pointer duration-200 dark:bg-gray-600 dark:text-white dark:font-bold dark:border-gray-700"
+        >
+          <div className="mr-4 text-lg text-[#9F8170] font-bold">
+            Open Counter
+          </div>
+          <BsArrowRight className="mt-0.5 font-black text-2xl text-[#9F8170]" />
+        </button>
 
-        <motion.div initial={{opacity: 0, y: -100}} animate={{opacity: 100, y: 0}}
-                    transition={{duration: 1, delay: 0.2, ease: "easeInOut"}} className='w-screen fixed top-0 -z-20'>
-            {/* Change the svg entirely when dark mode toggles */}
-            <img alt='brown waves' src={wavesmobile} className={`w-[100%] fixed`}/>
+        <div className="text-center text-lg mt-10 md:text-xl">
+          إِنَّ ٱللَّهَ يُحِبُّ ٱلتَّوَّٲبِينَ وَيُحِبُّ ٱلۡمُتَطَهِّرِينَ{" "}
+        </div>
+        <div className="text-center text-lg max-w-56 mx-auto mt-4">
+          "Verily, Allah loves those who repent and those who purify
+          themselves." [Al-Qur'an 2:222]
+        </div>
+      </div>
 
-        </motion.div>
-        <motion.img initial={{opacity: 0, y: -10}} animate={{opacity: 100, y: 0}}
-                    transition={{duration: 1.2, delay: 1.2}} src={lantern} className='fixed z-50 top-[135px] h-[70px]'/>
-        <motion.img initial={{opacity: 0, y: -10}} animate={{opacity: 100, y: 0}} src={lantern}
-                    transition={{duration: 1.2, delay: 1.6}} className='fixed z-50 top-[135px] right-0 h-[70px]'/>
-        <motion.img initial={{opacity: 0, y: -10}} animate={{opacity: 100, y: 0}} src={lantern}
-                    transition={{duration: 1.2, delay: 1.4}} className='fixed z-50 top-[165px] right-[50%] h-[70px]'/>
-        <motion.img initial={{opacity: 0, y: -10}} animate={{opacity: 100, y: 0}}
-                    transition={{duration: 1.2, delay: 1.3, ease: 'easeInOut'}} src={sparkles}
-                    className='fixed z-50 top-[135px] left-12'/>
-        <motion.img initial={{opacity: 0, y: -10}} animate={{opacity: 100, y: 0}} transition={{duration: 1.2, delay: 1.5, ease: 'easeInOut'}} src={sparkles} className='fixed z-50 top-[138px]  right-11'/>
-        <motion.img initial={{opacity: 0, y: -10}} animate={{opacity: 100, y: 0}} transition={{duration: 1.2, delay: 1.7, ease: 'easeInOut'}} src={sparkles} className='fixed z-50 top-[165px] right-[50%]'/>
-
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 100, y: 0 }}
+        transition={{ duration: 1, delay: 0.2, ease: "easeInOut" }}
+        className="w-screen fixed top-0 -z-20"
+      >
+        {/* Change the svg entirely when dark mode toggles */}
+        <img alt="brown waves" src={wavesmobile} className={`w-[100%] fixed`} />
+      </motion.div>
+      <motion.img
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 100, y: 0 }}
+        transition={{ duration: 1.2, delay: 1.2 }}
+        src={lantern}
+        className="md:hidden fixed z-50 top-[135px] h-[70px]"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 100, y: 0 }}
+        src={lantern}
+        transition={{ duration: 1.2, delay: 1.6 }}
+        className="md:hidden fixed z-50 top-[135px] right-0 h-[70px]"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 100, y: 0 }}
+        src={lantern}
+        transition={{ duration: 1.2, delay: 1.4 }}
+        className="md:hidden fixed z-50 top-[165px] right-[50%] h-[70px]"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 100, y: 0 }}
+        transition={{ duration: 1.2, delay: 1.3, ease: "easeInOut" }}
+        src={sparkles}
+        className="md:hidden fixed z-50 top-[135px] left-12"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 100, y: 0 }}
+        transition={{ duration: 1.2, delay: 1.5, ease: "easeInOut" }}
+        src={sparkles}
+        className="md:hidden fixed z-50 top-[138px]  right-11"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 100, y: 0 }}
+        transition={{ duration: 1.2, delay: 1.7, ease: "easeInOut" }}
+        src={sparkles}
+        className="md:hidden fixed z-50 top-[165px] right-[50%]"
+      />
     </div>
+  );
 }
